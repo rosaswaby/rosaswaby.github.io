32fb22c7a78983f5f1d898b4070dcbea87076b12
Rosa <rs4791@nyu.edu>
2016-04-22 11:47:05 -0400

    redesigning personal website

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  // when user clicks li.expand
+  $('#box1').click(function() {
+    // hide and show image with slide
+    $('#about').hide();
+    $('.navbar').hide().fadeIn(1000);
+  })
+
+})
