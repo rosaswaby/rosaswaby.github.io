f00bebc3b008b2a946301e7ad4daa26a7fdc1d06
Rosa <rs4791@nyu.edu>
2016-04-28 00:40:37 -0400

    Updating redesign - still need ideas for projects section

--- redesign_personal_website/my_jquery.js
+++ redesign_personal_website/my_jquery.js
@@ -4,14 +4,23 @@ $(document).ready(function() {
     $("img.WD").show();
     $("img.D").show();
 
-    $("#cs_button").css("background-color", "pink");
-    $("#wd_button").css("background-color", "pink");
-    $("#d_button").css("background-color", "pink");
+    $("#cs_button").css("background-color", "#ef9ea4");
+    $("#wd_button").css("background-color", "#ef9ea4");
+    $("#d_button").css("background-color", "#ef9ea4");
+
+    $("#my_skills").click(function() {
+        $("#wd_button").css("background-color", "#ef9ea4");
+        $("#d_button").css("background-color", "#ef9ea4");
+        $("#cs_button").css("background-color", "#ef9ea4");
+        $("img.CS").show();
+        $("img.WD").show();
+        $("img.D").show();
+    });
 
     $("#cs_button").click(function() {
         $("#wd_button").css("background-color", "white");
         $("#d_button").css("background-color", "white");
-        $("#cs_button").css("background-color", "pink");
+        $("#cs_button").css("background-color", "#ef9ea4");
         $("img.CS").show();
         $("img.WD").hide();
         $("img.D").hide();
@@ -20,7 +29,7 @@ $(document).ready(function() {
     $("#wd_button").click(function() {
         $("#cs_button").css("background-color", "white");
         $("#d_button").css("background-color", "white");
-        $("#wd_button").css("background-color", "pink");
+        $("#wd_button").css("background-color", "#ef9ea4");
         $("img.WD").show();
         $("img.CS").hide();
         $("img.D").hide();
@@ -29,7 +38,7 @@ $(document).ready(function() {
     $("#d_button").click(function() {
         $("#wd_button").css("background-color", "white");
         $("#cs_button").css("background-color", "white");
-        $("#d_button").css("background-color", "pink");
+        $("#d_button").css("background-color", "#ef9ea4");
         $("img.D").show();
         $("img.CS").hide();
         $("img.WD").hide();
