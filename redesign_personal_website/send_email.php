8495f6a507b2a8076481a79f42e2f3a197ee26f8
Rosa <rs4791@nyu.edu>
2016-04-29 02:24:00 -0400

    change email to be sent in send email pjp

--- redesign_personal_website/send_email.php
+++ redesign_personal_website/send_email.php
@@ -2,6 +2,7 @@
     $name=$_POST['name'];
     $email=$_POST['email'];
     $message=$_POST['message'];
+
     if (($name=="")||($email=="")||($message==""))
         {
         echo "All fields are required, please fill <a href=\"\">the form</a> again.";
@@ -9,7 +10,7 @@
     else{
         $from="From: $name<$email>\r\nReturn-path: $email";
         $subject="Message sent using your contact form";
-        mail("youremail@yoursite.com", $subject, $message, $from);
+        mail("rosa.swaby@nyu.edu", $subject, $message, $from);
         echo "Email sent!";
         }
 ?>
