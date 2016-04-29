7d7fa4bda0b1b0fe57378e5ab07561b09a2873da
Rosa <rs4791@nyu.edu>
2016-04-29 11:14:31 -0400

    ahhhh pt 2

---
+++ redesign_personal_website/contact_me_email_form.php
@@ -0,0 +1,18 @@
+<?php
+// $action=$_REQUEST['action'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+if (($name=="")||($email=="")||($message==""))
+    {
+    echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+    }
+
+else{
+    $headers="From: $name<$email>\r\nReturn-path: $email";
+    $subject="Message sent using your contact form";
+    $my_email = "rosa.swaby@nyu.edu";
+    mail($my_email, $subject, $message, $headers);
+    }
+?>
