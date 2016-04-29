632611af31ec0365cc0ca835b202607c00a15f6d
Rosa <rs4791@nyu.edu>
2016-04-29 11:11:10 -0400

    ahhhh

--- redesign_personal_website/send_email.php
+++ redesign_personal_website/send_email.php
@@ -1,14 +1,14 @@
 <?php
 // $action=$_REQUEST['action'];
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
+$name=$_GET['name'];
+$email=$_GET['email'];
+$message=$_GET['message'];
 
 if (($name=="")||($email=="")||($message==""))
     {
     echo "All fields are required, please fill <a href=\"\">the form</a> again.";
     }
-    
+
 else{
     $headers="From: $name<$email>\r\nReturn-path: $email";
     $subject="Message sent using your contact form";
