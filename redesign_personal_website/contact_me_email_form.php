be47a537ce5fccd64c52419c3b227e80345a6faa
Rosa <rs4791@nyu.edu>
2016-04-29 11:22:16 -0400

    just trying stuff

--- redesign_personal_website/contact_me_email_form.php
+++ redesign_personal_website/contact_me_email_form.php
@@ -1,18 +1,20 @@
 <?php
 // $action=$_REQUEST['action'];
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
+// $name=$_POST['name'];
+// $email=$_POST['email'];
+// $message=$_POST['message'];
+//
+// if (($name=="")||($email=="")||($message==""))
+//     {
+//     echo "All fields are required please!";
+//     }
+//
+// else{
+//     $headers="From: $name<$email>\r\nReturn-path: $email";
+//     $subject="Message sent using your contact form";
+//     $my_email = "rosa.swaby@nyu.edu";
+//     mail($my_email, $subject, $message, $headers);
+//     }
 
-if (($name=="")||($email=="")||($message==""))
-    {
-    echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-    }
-
-else{
-    $headers="From: $name<$email>\r\nReturn-path: $email";
-    $subject="Message sent using your contact form";
-    $my_email = "rosa.swaby@nyu.edu";
-    mail($my_email, $subject, $message, $headers);
-    }
+echo "PHP IS WORKING";
 ?>
