afcaa68981cb9a5e90f609570a70e607b633ab96
Rosa <rs4791@nyu.edu>
2016-04-29 10:46:05 -0400

    Updating send email

--- redesign_personal_website/send_email.php
+++ redesign_personal_website/send_email.php
@@ -1,16 +1,16 @@
 <?php
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $message=$_POST['message'];
-
-    if (($name=="")||($email=="")||($message==""))
-        {
-        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-        }
-    else{
-        $from="From: $name<$email>\r\nReturn-path: $email";
-        $subject="Message sent using your contact form";
-        mail("rosa.swaby@nyu.edu", $subject, $message, $from);
-        echo "Email sent!";
-        }
+$action=$_REQUEST['action'];
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$message=$_REQUEST['message'];
+if (($name=="")||($email=="")||($message==""))
+    {
+    echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+    }
+else{
+    $from="From: $name<$email>\r\nReturn-path: $email";
+    $subject="Message sent using your contact form";
+    mail("youremail@yoursite.com", $subject, $message, $from);
+    echo "Email sent!";
+    }
 ?>
