84924e2f1a1322aca2c0120a99d2cd909cea12bc
Rosa <rs4791@nyu.edu>
2016-05-29 21:47:11 -0400

    Working on project images

--- codepen-redesign/js/index.js
+++ codepen-redesign/js/index.js
@@ -2,10 +2,10 @@ function myFunction() {
   document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
 }
 
-$("#project1").hover(
-  function() {
-    $(this).attr("src", "http://cdn-6.dooziedog.com/dog_breeds/beagle/images/full/Beagle-Puppy.jpg");
-  },
-    function() {
-      $(this).attr("src", "http://i.imgur.com/Flk3FcN.png");
-  });
+// $("#project1").hover(
+//   function() {
+//     $(this).attr("src", "http://cdn-6.dooziedog.com/dog_breeds/beagle/images/full/Beagle-Puppy.jpg");
+//   },
+//     function() {
+//       $(this).attr("src", "http://i.imgur.com/Flk3FcN.png");
+//   });
