7cecfe1031598cb4693aa2fa5f3e5464e1ba9483
Rosa <rs4791@nyu.edu>
2016-05-29 20:29:57 -0400

    Updated Redesign of personal website using codepen

---
+++ codepen-redesign/js/index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
+
+$("#project1").hover(
+  function() {
+    $(this).attr("src", "http://cdn-6.dooziedog.com/dog_breeds/beagle/images/full/Beagle-Puppy.jpg");
+  },
+    function() {
+      $(this).attr("src", "http://i.imgur.com/Flk3FcN.png");
+  });
