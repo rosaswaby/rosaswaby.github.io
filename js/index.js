52698593c8d3a12b47555f54e839a193259a6965
Rosa <rs4791@nyu.edu>
2016-06-13 16:45:40 -0400

    Adding hover img src change to social media icons

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ function myFunction() {
   document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
 }
 
+function myFunction(social_media_image) {
+  var new_src = social_media_image.id + "_clicked.png";
+  social_media_image.src = new_src;
+}
+
 // $("#project1").hover(
 //   function() {
 //     $(this).attr("src", "http://cdn-6.dooziedog.com/dog_breeds/beagle/images/full/Beagle-Puppy.jpg");
