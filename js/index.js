0c44c534d64d1260406c7fec4618de79afb5416a
Rosa <rs4791@nyu.edu>
2016-07-01 17:54:05 -0400

    Debugging JS Code

--- js/index.js
+++ js/index.js
@@ -4,20 +4,22 @@ function navBarFunction() {
 
 function projects_hover(project_image) {
     var project_image_src = project_image.src;
+    alert(project_image.src);
     project_image_src =  project_image_src.split(".").pop();
     var new_src = project_image_src + "_hover.png";
     project_image.src = new_src;
     // testing out switching images
-    alert(project_image.src);
+    // alert(project_image.src);
 }
 
 function projects_mouseaway(project_image) {
   var project_image_src = project_image.src;
+  alert(project_image.src);
   project_image_src =   project_image_src.split("_hover").pop();
   var new_src = project_image_src + ".png";
   project_image.src = new_src;
   // testing out switching images
-  alert(project_image.src);
+  // alert(project_image.src);
 }
 
 // skills tab js
