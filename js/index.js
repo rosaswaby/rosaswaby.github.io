799de281a90d990ee878a821d94202452196a31a
Rosa <rs4791@nyu.edu>
2016-07-07 22:39:07 -0400

    testing out fading with project images

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ function projects_hover(project_image) {
     var project_image_src = project_image.src;
     project_image_src =  project_image_src.split(".png").shift();
     var new_src = project_image_src + "_hover.png";
+
     project_image.src = new_src;
 
 }
@@ -17,8 +18,12 @@ function projects_mouseaway(project_image) {
   project_image.src = new_src;
 }
 
-// skills tab js
+// fading for project images
+$("#project_images").hover(function(){
+    $("this").fadeOut();
+});
 
+// skills tab js
 $("#tab1_content").show();
 $("#tab2_content").hide();
 $("#tab3_content").hide();
